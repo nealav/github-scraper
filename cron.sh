841d0805c1225e2b39014f9a5a5a54735503b142
nealav <neal.viswanath@gmail.com>
2020-04-02 12:01:31 +0000

    scrape

--- cron.sh
+++ cron.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+PATH=/home/neal_viswanath/.nvm/versions/node/v12.16.1/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games
 
 case "$(pidof node | wc -w)" in 
 
 0) echo "Restarting Github Scraper: $(date)" >> /home/neal_viswanath/github-email-scraper/process.txt
-   cd /home/neal_viswanath/github-email-scraper/ && npm start
+   node /home/neal_viswanath/github-email-scraper/scraper.js
    ;;
 1) #Process is running
    echo "Process is running"
